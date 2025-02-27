520f9ee7bbd0c150aa2be2204987d124f3b715f6
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-02-27 13:01:32 +0530

    Testing

---
+++ Add.java
@@ -0,0 +1,33 @@
+class Xyz{
+static int x;
+static{
+  x=80;
+  System.out.println("Hy");
+}
+int y;
+{
+  y=80;
+  System.out.println("Bye");
+}
+
+}
+
+//import java.util.Scanner;
+class Add extends Xyz{
+	public static void main(String args[]){
+	 /*  Scanner sc=new Scanner(System.in);
+           System.out.println("Enter Fst Num");
+           int num1=sc.nextInt();
+           System.out.println("Enter Sec Num");
+           int num2=sc.nextInt();
+           int result=num1+num2;
+           System.out.println("Result is: "+result);
+	
+	long x=299111111111111L;
+        System.out.println(x);
+
+   */
+   
+        }
+
+}
